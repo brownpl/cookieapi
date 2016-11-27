90ebaa7812fc27ee8017ba10e830700babf41586
Patrick Brown <brownpl@gmail.com>
2016-11-26 20:14:20 -0800

    Adding more functionality

--- handler.js
+++ handler.js
@@ -4,38 +4,113 @@
 let AWS = require('aws-sdk');
 var db = new AWS.DynamoDB.DocumentClient();
 
+module.exports.getallcookies = (event, context, callback) => {
+		var params = {
+				TableName: 'CookieTable'
+		};
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-  callback(null, response);
+		db.scan(params, function(err, data) {
+				if (err) callback(err, null);
+				else {
+						const response = {
+								statusCode: 200,
+								body: JSON.stringify(data.Items)
+						};
+						callback(null, response);
+				}
+		});
 };
 
-module.exports.addentry = (event, context, callback) => {
+module.exports.getcookies = (event, context, callback) => {
+		var params = {
+				TableName: "CookieTable",
+				KeyConditionExpression: "#UserId = :UserId",
+				ExpressionAttributeNames: {
+						"#UserId": "UserId"
+				},
+				ExpressionAttributeValues: {
+						":UserId": event.pathParameters.UserId
+				}
+		};
 
+		db.query(params, (err, data) => {
+				if (err) {
+						const err = {
+								statusCode: 200,
+								body: JSON.stringify({
+										input: event
+								})
+						};
+						callback(err, null);
+				} else {
+						const response = {
+								statusCode: 200,
+								body: JSON.stringify(data.Items)
+						};
+						callback(null, response);
+				}
+		});
+};
+module.exports.setcookies = (event, context, callback) => {
+
+		var params = {
+				TableName: 'CookieTable',
+				Item: JSON.parse(event.body)
+		};
+
+		db.put(params, function(err, data) {
+				if (err) console.log(err);
+				else console.log(data);
+		});
+		const response = {
+				statusCode: 200,
+				body: JSON.stringify({
+						success: true
+				}),
+		};
+		callback(null, response);
+};
+
+module.exports.getcookies = (event, context, callback) => {
+		var params = {
+				TableName: 'CookieTable'
+		};
+
+		db.scan(params, function(err, data) {
+				if (err) callback(err, null);
+				else {
+						const response = {
+								statusCode: 200,
+								body: JSON.stringify(data.Items)
+						};
+						callback(null, response);
+				}
+		});
+};
+
+module.exports.addacookie = (event, context, callback) => {
 	var params = {
-	  TableName : 'cookieTable',
-	  Item: {
-	     user_id: '1asdfasdf',
-			 cookies: 5
-	  }
+		TableName: "CookieTable",
+			Key: { "UserId": event.pathParameters.UserID },
+			UpdateExpression: "SET cookies = :cookies + 1",
 	};
 
-	db.put(params, function(err, data) {
-	  if (err) console.log(err);
-	  else console.log(data);
-	});
-	const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'written database',
-      input: event,
-    }),
-  };
-  callback(null, response);
+		db.update(params, (err, data) => {
+				if (err) {
+						const err = {
+								statusCode: 200,
+								body: JSON.stringify({
+										input: event
+								})
+						};
+						callback(err, null);
+				} else {
+
+						const response = {
+								statusCode: 200,
+								body: JSON.stringify(data)
+						};
+						callback(null, response);
+				}
+		});
 };
